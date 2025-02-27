31d35f6cf20eab893aa447c46c660f7451315521
Sivanithipy <pyshivanithi@gmail.com>
2025-02-27 21:22:25 +0530

    53. Maximum Subarray

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,7 +1,7 @@
 class MaximumSubarray {
   public int maxSubArray(int[] nums) {
       int sum=0;
-      int max=0;
+      int max=nums[0];
       for(int i=0;i<nums.length;i++)
       {
          sum=sum+nums[i];
