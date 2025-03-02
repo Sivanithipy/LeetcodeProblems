2967659c3d3af9649e20df513f693f4585da6e12
Sivanithipy <pyshivanithi@gmail.com>
2025-03-02 23:13:38 +0530

    153. Find Minimum in Rotated Sorted Array

---
+++ findminimuminrotatedsortedarray.java
@@ -0,0 +1,16 @@
+class findminimuminrotatedsortedarray {
+  public int findMin(int[] nums) {
+      int min=nums[0];
+      for(int i=0;i<nums.length;i++)
+      {
+          if(nums[i]<min)
+          {
+              min=nums[i];
+          }
+      }
+      return min;
+  }
+}
+
+  
+
