be3962510075679101733814e02f7dbed37b2040
Sivanithipy <pyshivanithi@gmail.com>
2025-03-04 22:41:15 +0530

    26. Remove Duplicates from Sorted Array

--- removeduplicatefromsortedarray.java
+++ removeduplicatefromsortedarray.java
@@ -1,19 +1,15 @@
-public class removeduplicatefromsortedarray {
-  public int removeDuplicates(int[] nums) {
-      int k=1;
-      
-          if(nums.length==0){
-              return 0;
-          }
-          for(int i=1;i<nums.length;i++){
-             if(nums[i]!=nums[i-1]){
-              nums[k]=nums[i];
-              k++;
-             }
-          }
-      
-      return k;
-  }
-} {
-  
+class removeduplicatefromsortedarray {
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+        for(int j=0;j<nums.length;j++)
+        {
+        if(nums[j]!=nums[i])
+        {
+            i++;
+            nums[i]=nums[j];
+        }
+        }
+        return i+1;
+    }
 }
+ 
