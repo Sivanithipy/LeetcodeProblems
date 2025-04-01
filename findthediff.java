7371ec9ee30ffbf61b3c75f2131ad6ffe579ca9e
Sivanithipy <pyshivanithi@gmail.com>
2025-04-01 11:21:31 +0530

    389. Find The Difference

---
+++ findthediff.java
@@ -0,0 +1,14 @@
+class findthediff {
+  public char findTheDifference(String s, String t) {
+     int r=0;
+     for(char c:s.toCharArray())
+     {
+      r ^=c;
+     }
+      for(char c:t.toCharArray())
+     {
+      r ^=c;
+     }
+     return (char)r;
+  }
+}
