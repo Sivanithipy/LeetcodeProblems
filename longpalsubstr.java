032d3d2a31d50bd07e0ec41cbfd954eb76430d69
Sivanithipy <pyshivanithi@gmail.com>
2025-05-05 11:39:23 +0530

    5. Longest Palindromic Substring

---
+++ longpalsubstr.java
@@ -0,0 +1,25 @@
+class longpalsubstr {
+    public String longestPalindrome(String s) {
+        String longest = "";
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                String sub = s.substring(i, j + 1);
+                if (isPalindrome(sub) && sub.length() > longest.length()) {
+                    longest = sub;
+                }
+            }
+        }
+        return longest;
+    }
+    private boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
