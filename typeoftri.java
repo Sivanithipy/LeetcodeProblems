5d72ab64c6b2e030402a7a6b1d77df95c40f3173
Sivanithipy <pyshivanithi@gmail.com>
2025-05-19 10:43:03 +0530

    3024. Type of Triangle

---
+++ typeoftri.java
@@ -0,0 +1,26 @@
+class typeoftri {
+    public String triangleType(int[] nums) {
+       if(nums.length!=3)
+       {
+        return "invalid";
+       }
+       int a=nums[0],b=nums[1],c=nums[2];
+       if(a+b<=c||a+c<=b||b+c<=a)
+       {
+        return "none";
+       }
+       if(a==b&b==c)
+       {
+        return "equilateral";
+       }
+       else if(a==b||b==c||c==a)
+       {
+        return "isosceles";
+       }
+       else
+       {
+        return "scalene";
+       }
+
+    }
+}
