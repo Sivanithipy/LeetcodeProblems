c80219c51f5636331172d7ee5dbbe28521fc4736
Sivanithipy <pyshivanithi@gmail.com>
2025-05-24 09:52:11 +0530

    2942. Find Words Containing Character

---
+++ findwordsconchar.java
@@ -0,0 +1,13 @@
+class findwordsconchar {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> a=new ArrayList<>();
+        for(int i=0;i<words.length;i++)
+        {
+            if(words[i].indexOf(x)!=-1)
+            {
+                a.add(i);
+            }
+        }
+        return a;
+    }
+}
