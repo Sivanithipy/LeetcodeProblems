b040a8316bbe36b65704d7a911bab5cab6a14f00
Sivanithipy <pyshivanithi@gmail.com>
2025-06-02 09:18:36 +0530

    1832. Check if the Sentence Is Pangram

---
+++ checkifthesenispangram.java
@@ -0,0 +1,16 @@
+class checkifthesenispangram {
+    public boolean checkIfPangram(String sentence) {
+       if(sentence.length()<26) return false;
+       boolean[] seen=new boolean[26];
+       for(char c:sentence.toCharArray())
+       {
+        seen[c-'a']=true;
+       }
+       for(boolean b:seen)
+       {
+        if(!b) return false;
+
+       }
+       return true;
+    }
+}
